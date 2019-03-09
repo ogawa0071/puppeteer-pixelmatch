1a40815206cafb565debdac819a45a7716b4b966
Futa Ogawa <ogawa0071@gmail.com>
2019-03-10 07:37:28 +0900

    Create.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import {Command, flags} from '@oclif/command'
+import * as fs from 'fs'
+import * as puppeteer from 'puppeteer'
+import * as url from 'url'
+const compareImages = require('resemblejs/compareImages')
 
 class PuppeteerPixelmatch extends Command {
   static description = 'describe the command here'
@@ -7,22 +11,79 @@ class PuppeteerPixelmatch extends Command {
     // add --version flag to show CLI version
     version: flags.version({char: 'v'}),
     help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
+    // flag with a value (-s, --source=VALUE)
+    source: flags.string({char: 's', description: 'compare source', required: true}),
+    // flag with a value (-t, --target=VALUE)
+    target: flags.string({char: 't', description: 'compare target', required: true}),
   }
 
-  static args = [{name: 'file'}]
+  static args = [{
+    name: 'pathsArray',
+    required: true
+  }]
 
   async run() {
     const {args, flags} = this.parse(PuppeteerPixelmatch)
 
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from ./src/index.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
+    const source = flags.source
+    // tslint:disable-next-line no-http-string
+    const target = flags.target
+    const pathsArrayPath = args.pathsArray
+    const pathsArray = JSON.parse(await fs.promises.readFile(pathsArrayPath, 'utf8'))
+
+    for (let index = 0; index < pathsArray.length; index++) {
+      const path = pathsArray[index]
+
+      this.log(`${index}/${pathsArray.length}`)
+
+      // スクリーンショットを作成
+      const sourceUrl = new URL(path, source)
+      const targetUrl = new URL(path, target)
+      const sourceScreenshot = await this.screenshot(sourceUrl)
+      const targetScreenshot = await this.screenshot(targetUrl)
+
+      // 実行しているパスにフォルダーを作成し保存
+      await fs.promises.mkdir(`${this.compatibleUrl(sourceUrl.host)}`, {
+        recursive: true
+      })
+      await fs.promises.mkdir(`${this.compatibleUrl(targetUrl.host)}`, {
+        recursive: true
+      })
+      await fs.promises.writeFile(`${this.compatibleUrl(sourceUrl.host)}/${this.compatibleUrl(sourceUrl.pathname)}.png`, sourceScreenshot)
+      await fs.promises.writeFile(`${this.compatibleUrl(targetUrl.host)}/${this.compatibleUrl(targetUrl.pathname)}.png`, targetScreenshot)
+
+      // SourceとTargetを比較
+      const diff = await compareImages(
+        sourceScreenshot,
+        targetScreenshot
+      )
+      const diffScreenshot = diff.getBuffer()
+
+      // 実行しているパスにフォルダーを作成し保存
+      await fs.promises.mkdir('diff', {
+        recursive: true
+      })
+      await fs.promises.writeFile(`diff/${this.compatibleUrl(sourceUrl.pathname)}.png`, diffScreenshot)
     }
+  }
+
+  async screenshot(url: url.URL) {
+    const browser = await puppeteer.launch()
+
+    const page = await browser.newPage()
+    await page.goto(url.toString())
+
+    const screenshot = await page.screenshot({
+      fullPage: true
+    })
+
+    await browser.close()
+
+    return screenshot
+  }
+
+  compatibleUrl(url: string) {
+    return url.replace(/\W/g, '_')
   }
 }
 
--- test/index.test.ts
+++ test/index.test.ts
@@ -3,17 +3,17 @@ import {expect, test} from '@oclif/test'
 import cmd = require('../src')
 
 describe('puppeteer-pixelmatch', () => {
-  test
-    .stdout()
-    .do(() => cmd.run([]))
-    .it('runs hello', ctx => {
-      expect(ctx.stdout).to.contain('hello world')
-    })
+  // test
+  //   .stdout()
+  //   .do(() => cmd.run([]))
+  //   .it('runs hello', ctx => {
+  //     expect(ctx.stdout).to.contain('hello world')
+  //   })
 
-  test
-    .stdout()
-    .do(() => cmd.run(['--name', 'jeff']))
-    .it('runs hello --name jeff', ctx => {
-      expect(ctx.stdout).to.contain('hello jeff')
-    })
+  // test
+  //   .stdout()
+  //   .do(() => cmd.run(['--name', 'jeff']))
+  //   .it('runs hello --name jeff', ctx => {
+  //     expect(ctx.stdout).to.contain('hello jeff')
+  //   })
 })
